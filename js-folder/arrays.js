c0b6359f9333c1309bd6db9faac8f05825aee9f2
Nikhil <ns45202020@gmail.com>
2026-04-10 22:22:24 +0530

    deleting non use file

--- js-folder/arrays.js
+++
@@ -1,8 +0,0 @@
-const todoList = [];
-function AddTodo () {
-  const inputElement = document.querySelector('.js-placeholder-name')
-  const name = inputElement.value;
- 
-  todoList.push(name);
-  console.log(todoList)
-}
